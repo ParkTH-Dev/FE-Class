ef677f7d74721245f4a41cb46d8b6f986cc239e2
ParkTH-Dev <xotj2635@naver.com>
2024-07-12 00:02:47 +0900

    240711

--- 240628(customizing)/roblox/script.js
+++ 240628(customizing)/roblox/script.js
@@ -1,14 +1,3 @@
-// 1. 웹브라우저에게 클릭할 아벤트 대상을 알려준다.
-// quertSelector
-// addEventListener
-
-// 2. 클릭을 했을 때, css로 정의해놓은 가상클래스를 원하는 요소에 적용시킨다.
-// classList
-// add() || remove()
-// button => 기본속성 / 무력화 preventDfault()
-
-// 2_1. 가상클래스 = filledA & filledB
-
 const femaleBtn = document.querySelector("#femalebtn");
 const maleBtn = document.querySelector("#malebtn");
 
