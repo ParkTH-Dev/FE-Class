929638020055b2d837140fb213f69935ed45f421
ParkTH-Dev <xotj2635@naver.com>
2024-07-17 01:07:41 +0900

    240716

--- 240715/02 flex/script.js
+++ 240715/02 flex/script.js
@@ -1,3 +1,5 @@
+// Header Nav
+
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   const header = document.querySelector("header");
@@ -7,4 +9,41 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("active");
   }
+});
+
+// slick slider
+$(".myslider").slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
