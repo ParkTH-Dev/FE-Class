6b18467e235232f36a565961fa463dd0302aa5da
ParkTH-Dev <xotj2635@naver.com>
2024-07-29 20:11:55 +0900

    240729

--- 240723/script.js
+++ 240723/script.js
@@ -1,3 +1,35 @@
+// header
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  let scrollY = window.scrollY;
+  if (scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+// trigger event
+const trigger = document.querySelector(".trigger");
+trigger.addEventListener("click", function () {
+  const gnb = document.querySelector(".gnb");
+  const menus = document.querySelectorAll(".menu a");
+
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+
+  menus.forEach((menu) => {
+    menu.addEventListener("click", () => {
+      trigger.classList.remove("active");
+      gnb.classList.remove("active");
+    });
+  });
+});
+// ScrollTo
+$(".menu a").click(function () {
+  $.scrollTo(this.hash || 0, 600);
+});
+
 // slick slider : history
 $(".history-slider").slick({
   dots: true,
@@ -5,6 +37,29 @@ $(".history-slider").slick({
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 // slickslider : Project
 $(".project-photo").slick({
