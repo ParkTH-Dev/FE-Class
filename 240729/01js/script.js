fdee6e633774c02a0f447591705db9ba4e0e4137
ParkTH-Dev <xotj2635@naver.com>
2024-07-29 20:11:55 +0900

    240729

---
+++ 240729/01js/script.js
@@ -0,0 +1,20 @@
+// const num = 1;
+// const num2 = 2;
+
+// const sum = num + num2;
+// console.log(sum);
+
+// const str1 = "1";
+// const str2 = "2";
+
+const num = 1000;
+const str = "원";
+const counter = num + str;
+console.log(counter);
+
+const num1 = 1000;
+const str1 = "2000";
+const counter1 = num1 + str1;
+console.log(counter1);
+
+prompt();
