d2c7ec5dc995dabec1f2b8d7acee4d10bf9df7ac
ParkTH-Dev <xotj2635@naver.com>
2024-07-30 21:56:43 +0900

    240730

---
+++ 240730/01js/script.js
@@ -0,0 +1,55 @@
+// // const jsBook = {
+// //   // 속성: property
+// //   // key: value
+// //   title: "지옥에서 온 깃 허브",
+// //   pages: 330,
+// // };
+
+// // // 객체안에 담긴 값을 찾아올 수 있음
+// // // 온점 표기법
+// // const jsBookTitle = jsBook.title;
+// // console.log(jsBookTitle);
+// // // 대괄호 표기법
+// // const jsBookPages = jsBook["pages"];
+// // console.log(jsBookPages);
+
+// // jsBook.title = "천국에서 온 깃 허브";
+// // console.log(jsBook);
+
+// // const arr = ["red", "green", "blue"];
+
+// // console.log(arr[arr.length - 1]);
+
+// // let test01 = Symbol();
+// // let test02 = Symbol();
+
+// // console.log(test01 === test02);
+
+// // let id = Symbol("userId");
+// // const member = {
+// //   name: "David",
+// //   [id]: 12345,
+// // };
+
+// // console.log(member);
+
+// // //
+// // const fncTest = () => {
+// //   console.log("click");
+// // };
+
+// const fru = ["apple", "banana", "cherry"];
+// const fav = [...fru];
+
+// fav[0] = "grape";
+
+// console.log(fru);
+
+// const num = null;
+
+// console.log(typeof num.toString());
+
+// console.log(Boolean(0));
+
+const num = prompt("자연수를 입력해주세요");
+console.log(num);
