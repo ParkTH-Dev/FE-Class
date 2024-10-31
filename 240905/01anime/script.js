a12a3a4e7808e52b40d83f23a8ff6c405fe39bd3
ParkTH-Dev <xotj2635@naver.com>
2024-10-31 20:29:43 +0900

    241031

--- 240905/01anime/script.js
+++ 240905/01anime/script.js
@@ -1,8 +1,8 @@
 anime({
   targets: ".svg2 path",
-  strokeDashoffset: [anime.setDashoffset, 100],
-  duration: 10000,
-  easing: "easeInOutSine",
-  //   loop: true,
-  //   direction: "alternate",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  duration: 8000,
+  easing: "easeInOutQuad",
+  loop: true,
+  direction: "alternate",
 });
