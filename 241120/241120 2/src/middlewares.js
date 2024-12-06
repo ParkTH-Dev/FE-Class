6243effd688cd92e6fc153cae568dddb007522c0
ParkTH-Dev <xotj2635@naver.com>
2024-12-06 13:25:53 +0900

    241206

---
+++ 241120/241120 2/src/middlewares.js
@@ -0,0 +1,36 @@
+import multer from "multer";
+
+export const localMiddleware = (req, res, next) => {
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.siteName = "Youtube";
+  res.locals.loggedInUser = req.session.user || {};
+  res.locals.avatarUrl = req.session.user.avatarUrl;
+  res.locals.videoUrl = req.session.user.videoUrl;
+  console.log(req.session.user);
+  next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
+
+export const avatarUpload = multer({
+  dest: "uploads/avatars/",
+  limits: { fileSize: 30000000 },
+});
+export const videoUpload = multer({
+  dest: "uploads/videos/",
+  limits: { fileSize: 10000000 },
+});
